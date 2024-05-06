734f6eaed974de71698f40c343ae2b3bad891a7a
mxkooo <mikoturko@wp.pl>
2024-05-06 17:48:18 +0200

    First commit

---
+++ src/main/java/io/github/mxkooo/flashcardapp/FlashcardAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.mxkooo.flashcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mxkooo/flashcardapp/FlashcardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mxkooo.flashcardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
