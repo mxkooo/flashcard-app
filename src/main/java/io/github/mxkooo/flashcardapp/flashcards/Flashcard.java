fe6f4ab19ad12c3d66b0060414be2f2d00e7d8e5
mxkooo <mikoturko@wp.pl>
2024-05-25 22:17:03 +0200

    Adding flashcard to database

--- src/main/java/io/github/mxkooo/flashcardapp/flashcards/Flashcard.java
+++ src/main/java/io/github/mxkooo/flashcardapp/flashcards/Flashcard.java
@@ -1,11 +1,9 @@
 package io.github.mxkooo.flashcardapp.flashcards;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
+@Table(name = "FLASHCARD")
 @Entity
 @NoArgsConstructor
 @Builder
